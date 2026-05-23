5a4545646d46e424ae6994967c43a0fffa6c8d8b
Mohamed Samir <m7mdsamir850@gmail.com>
2026-05-24 00:14:29 +0300

    Update db.js

--- js/db.js
+++ js/db.js
@@ -2,7 +2,7 @@ window.App = window.App || {};
 App.DB = (function() {
     const DB_KEY = 'DarkDrive_Data';
     const defaultData = {
-        users: [ { id: 'admin_1', username: 'admin', password: '@Admin123', role: 'admin', createdAt: Date.now() }, { id: 'user_1', username: 'demo', password: 'password123', role: 'user', createdAt: Date.now() } ],
+        users: [ { id: 'admin_1', username: 'admin', password: '@Admin123', role: 'admin', createdAt: Date.now() }, { id: 'user_1', username: 'demo', password: '123', role: 'user', createdAt: Date.now() } ],
         folders: [ { id: 'f_1', name: 'Cinematics', ownerId: 'admin_1', parentId: 'root' }, { id: 'f_2', name: 'Tutorials', ownerId: 'admin_1', parentId: 'root' } ],
         videos: [ ], shares: [ ]
     };
